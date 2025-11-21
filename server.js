68209628fff581f99b249277e3bf93dd10d93299
Venu <sj5384763@gmail.com>
2025-11-21 10:40:04 +0000

    Add simple backend for hosting assignment

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => res.send('Backend running'));
+
+app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
